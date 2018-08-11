be9ca55ea137b5c12fa3bcc4eedf6045f37d21f5
Fmressel <franzmichael@resselhaus.com>
2018-08-11 01:02:33 -0700

    Add basic ship building functionality

--- src/lib/section/ships.js
+++ src/lib/section/ships.js
@@ -1,19 +1,32 @@
 "use strict";
 
-// Time to build, in seconds
-// const buildTime = 5;
+const buildTimeInSeconds = 5,
+  ticksPerSecond = 60;
+let currentBuildTime = 0;
 
 module.exports = {
   setup() {
-    $("#build-ship").click(this.buildShip.bind(this));
+    $("#build-ship").click(this.startBuild.bind(this));
+    this.ship = $(".ship")[0];
   },
   
   tick() {
-    
+    if(currentBuildTime > 0) {
+      currentBuildTime--;
+
+      if(currentBuildTime === 0) {
+        this.deliverShip();
+      }
+    }
   },
   
-  buildShip() {
-    console.log("Build Ship!");
-    // TODO: Start building a ship
+  startBuild() {
+    currentBuildTime = buildTimeInSeconds * ticksPerSecond;
+    console.log("Started building ship!");
+  },
+
+  deliverShip() {
+    $(".ship").toggleClass("built");
+    console.log("Ship has been delivered!");
   }
 };
