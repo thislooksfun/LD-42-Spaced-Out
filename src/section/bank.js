7729f453ca0dc83632aa1e6b4a95bfa54d250755
thislooksfun <thislooksfun@repbot.org>
2018-08-13 15:26:47 -0500

    Add visualization of cashflow

--- src/section/bank.js
+++ src/section/bank.js
@@ -6,10 +6,48 @@ const startingMoney = 100000;
 
 
 var money = -1;
+let $bank = $("#bank");
 let $balance = $("#balance");
 function redraw() {
   $balance.text("$" + prettyPrint(money));
 }
+
+const timeBetweenFlows = 250;
+var lastFlow = 0;
+var queue = [];
+function queueCashflow($el) {
+  queue.push($el);
+  
+  if (queue.length === 1) {
+    let timeSinceLastFlow = Date.now() - lastFlow;
+    if (timeSinceLastFlow >= timeBetweenFlows) {
+      animateCashflow(queue.shift());
+    } else {
+      setTimeout(function() {
+        animateCashflow(queue.shift());
+      }, timeBetweenFlows - timeSinceLastFlow);
+    }
+  }
+}
+function animateCashflow($el) {
+  lastFlow = Date.now();
+  
+  $bank.append($el);
+  setTimeout(function() {
+    
+    setTimeout(function() {
+      if (queue.length > 0) {
+        animateCashflow(queue.shift());
+      }
+    }, timeBetweenFlows);
+    
+    $el.addClass("animate");
+    setTimeout(function() {
+      $el.remove();
+    }, 1250);
+  }, 50);
+}
+
 
 module.exports = {
   
@@ -30,6 +68,12 @@ module.exports = {
     if (money < 0) {
       require("../game").end("Out of money!");
     }
+    
+    queueCashflow($("<span>", {
+      class: "cashflow out",
+      text: "-$" + prettyPrint(cost)
+    }));
+    
     redraw();
   },
   
@@ -37,6 +81,12 @@ module.exports = {
     if (amount < 0) {
       return this.spend(-amount);
     }
+    
+    queueCashflow($("<span>", {
+      class: "cashflow in",
+      text: "+$" + prettyPrint(amount)
+    }));
+    
     money += amount;
     redraw();
   },
