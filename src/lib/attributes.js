707527e1939351d1e8d22b8037147e85d73a4020
thislooksfun <thislooksfun@repbot.org>
2018-08-13 00:19:27 -0500

    Start work on final attribute system

--- src/lib/attributes.js
+++ src/lib/attributes.js
@@ -7,12 +7,14 @@ module.exports = {
     var out = [];
     if (list == null) {
       // Make a copy of the array
-      list = this.list.slice(this.list);
+      list = this.groups.slice(this.groups);
     }
     
     while (count > 0 && list.length > 0) {
       let index = utils.rand(list.length - 1);
-      out.push(list[index]);
+      let entry = list[index];
+      let opt = entry.opts[utils.rand(entry.opts.length - 1)];
+      out.push(entry.name + "." + opt);
       list.splice(index, 1);
       count--;
     }
@@ -21,32 +23,18 @@ module.exports = {
     return out;
   },
   
-  list: [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
+  groups: [
+    {name: "clouds", opts: [ "none", "low", "med", "high" ]},
+    {name: "grass",  opts: [ "none", "low", "med", "high" ]},
+    {name: "desert", opts: [ "none", "some" ]},
+    {name: "cats",   opts: [ "none", "some" ]},
+    {name: "temperature", opts: [ "cold", "moderate", "hot" ]},
+    {name: "snow", opts: [ "none", "some" ]},
+    {name: "rain", opts: [ "none", "some" ]},
+    {name: "storms", opts: [ "none", "some" ]},
+    {name: "moons", opts: [ "none", "one", "two" ]},
+    {name: "rings", opts: [ "none", "some" ]},
+    {name: "planet-size", opts: [ "small", "large" ]},
   ],
+  
 };
