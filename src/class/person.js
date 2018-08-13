75eca18534c3c96d276a95a9ca2ba8a808654cd3
thislooksfun <thislooksfun@repbot.org>
2018-08-12 21:25:45 -0500

    Add money info to people and prevent overflow

--- src/class/person.js
+++ src/class/person.js
@@ -7,6 +7,11 @@ const diffScale = require("../section/score").diffScale;
 
 const maxAttributes = 5;
 
+const moneyScale = 10;
+const payoutMin  = 500;
+const fineMin    = 50;
+const bonusMin   = 25;
+
 
 function dragStart(e, id) {
   $(this).addClass("dragging");
@@ -34,11 +39,21 @@ module.exports = class Person {
   constructor() {
     this.id = util.uuidv4();
     
-    let scaledAttrMax =  Math.floor(1 + ((maxAttributes - 1) * diffScale()));
+    // let scaledAttrMax = Math.floor(1 + ((maxAttributes - 1) * diffScale()));
+    let scaledAttrMax = Math.floor(1 + ((maxAttributes - 1)));
     let max = Math.min(scaledAttrMax, maxAttributes);
     
     this.needs   = attributes.random(util.rand(max));
     this.desires = attributes.random(util.rand(max), this.needs.unused);
+    
+    
+    // Subtract one so that the max is `payoutMin + (payoutMin * (moneyScale-1)) = payoutMin * moneyScale`
+    // rather than `payoutMin + (payoutMin * moneyScale)`
+    let ms = moneyScale - 1;
+    this.payout = payoutMin + util.rand(Math.floor(ms * payoutMin * this.needs.length   / maxAttributes));
+    this.fine   = payoutMin + util.rand(Math.floor(ms * fineMin   * this.needs.length   / maxAttributes));
+    this.bonus  = payoutMin + util.rand(Math.floor(ms * bonusMin  * this.desires.length / maxAttributes));
+    
     
     this.$el = $("<div>", { class: "person" });
   }
@@ -65,6 +80,7 @@ module.exports = class Person {
         $needs.append($("<span>", { text: req }));
       }
     }
+    this.$el.append($needs);
     
     let $desires = $("<div>", {
       class: "desires",
@@ -76,8 +92,28 @@ module.exports = class Person {
         $desires.append($("<span>", { text: req }));
       }
     }
+    this.$el.append($desires);
     
-    this.$el.append($needs, $desires);
+    
+    
+    let $money = $("<div>", {
+      class: "money",
+    });
+    $money.append($("<span>", { class: "payout", text: "Payout: $" + util.prettyPrint(this.payout) }));
+    let $fine = $("<span>", { class: "fine", text: "Fine: $" + (util.prettyPrint(this.fine)) });
+    if (this.needs.length === 0) {
+      $fine.addClass("na");
+      $fine.text("Fine: N/A");
+    }
+    let $bonus = $("<span>", { class: "bonus", text: "Bonus: $" + util.prettyPrint(this.bonus) });
+    if (this.desires.length === 0) {
+      $bonus.addClass("na");
+      $bonus.text("Bonus: N/A");
+    }
+    $money.append($fine, $bonus);
+    this.$el.append($money);
+    
+    console.log(this.id + " -- payout: $" + this.payout + "; fine: $" + this.fine + "; bonus: $" + this.bonus);
     
     return this.$el;
   }
