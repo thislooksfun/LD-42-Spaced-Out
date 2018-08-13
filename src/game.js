e7b6ef0214ac2c5445b523d0d68efe9bd75f62a1
thislooksfun <thislooksfun@repbot.org>
2018-08-13 14:45:01 -0500

    Check if 'game_version' is defined, and fall back if not

--- src/game.js
+++ src/game.js
@@ -35,7 +35,7 @@ module.exports = {
   begin() {
     $overlayContainer.empty();
     $overlayContainer.append($("<h1>", {text: "Spaced Out"}));
-    $overlayContainer.append($("<span>", {text: "v" + game_version}));
+    $overlayContainer.append($("<span>", {text: "v" + (window.game_version || ".dev")}));
     let $startBtn = $("<button>", {text: "Start Game"});
     $startBtn.click(function() {
       $overlay.detach();
